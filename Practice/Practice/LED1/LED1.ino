3482ac78b5938dde6908c074c0d114366696b5e9
Takakazu Sato <38308536+Takakazu-Sato@users.noreply.github.com>
2018-06-01 16:18:36 +0900

    Add files via upload

---
+++ Practice/Practice/LED1/LED1.ino
@@ -0,0 +1,17 @@
+#define LED_PIN 10
+#define LED_SWITCH 2
+
+void setup() {
+  pinMode(LED_SWITCH,INPUT);
+  pinMode(LED_PIN,OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(LED_SWITCH) == HIGH){
+     digitalWrite(LED_PIN,HIGH);
+     delay(500);
+  }else{
+     digitalWrite(LED_PIN,LOW);
+     delay(500);
+  }
+}
---
+++ Practice/Practice/Puls_LED/Puls_LED.ino
@@ -0,0 +1,25 @@
+//LEDをつなぐピンを定義
+#define LED_PIN1 13
+#define LED_PIN2 10
+
+void setup() {
+  //LEDのピンを出力に
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+}
+
+//メインループ
+void loop() {
+  //LED1を点灯
+  digitalWrite(LED_PIN1, HIGH);
+  //LED2を点灯
+  digitalWrite(LED_PIN2, HIGH);
+  //1ミリ秒遅延
+  delay(1);
+  //LED1を消灯
+  digitalWrite(LED_PIN1, LOW);
+  //LED2を点灯
+  digitalWrite(LED_PIN2, HIGH);
+  //15ミリ秒遅延
+  delay(1);
+}
